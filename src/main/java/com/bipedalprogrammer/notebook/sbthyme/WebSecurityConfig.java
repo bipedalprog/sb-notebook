ead979a98f893b0f8fb18aadb4062d8da2ab197c
Dennis Caldwell <dennis@bipedalprogrammer.com>
2019-03-15 21:57:31 -0400

    Cleaned up login.

--- src/main/java/com/bipedalprogrammer/notebook/sbthyme/WebSecurityConfig.java
+++ src/main/java/com/bipedalprogrammer/notebook/sbthyme/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -45,6 +46,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new StandardPasswordEncoder("lazzyfox");
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
